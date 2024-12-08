c605b70e4ed09b78b6afd2daf9d5bd52577f6ea3
me-mega-noob-4044 <testtrolium@gmail.com>
2024-12-07 22:49:43 -0500

    ws server

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
-const { WebSocket } = require("ws");
+const WebSocket = require("ws");
 
 const app = express();
+const server = app.listen(3030, () => {
+    console.log("Server listening on port 3030");
+});
 
-app.listen(3030, () => {
-    console.log("Server listening to port 3030");
+const WebSocketServer = new WebSocket.WebSocketServer({ noServer: true });
+
+WebSocketServer.on("connection", (ws, request) => {
+    ws.on("message", (msg) => {});
+
+    ws.on("close", () => {});
+});
+
+server.on("upgrade", (request, socket, head) => {
+    WebSocketServer.handleUpgrade(request, socket, head, (ws) => {
+        WebSocketServer.emit("connection", ws, request);
+    });
 });

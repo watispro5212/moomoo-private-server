3360415cc90bcf4a427206ea066fdd4ada283160
me-mega-noob-4044 <testtrolium@gmail.com>
2025-03-30 23:01:37 -0400

    a

--- src/logic/ObjectManager.js
+++ src/logic/ObjectManager.js
@@ -3,6 +3,7 @@ import { gameObjects, players } from "../../index.js";
 import Packets from "../constants/Packets.js";
 import config from "../constants/config.js";
 import UTILS from "../constants/utils.js";
+import Player from "./Players.js";
 
 export default class ObjectManager {
     static grids = {};
@@ -10,7 +11,7 @@ export default class ObjectManager {
     static setObjectGrids(tmpObj) {
         let tmpS = 1440;
         let objX = Math.min(config.mapScale, Math.max(0, tmpObj.x));
-		let objY = Math.min(config.mapScale, Math.max(0, tmpObj.y));
+        let objY = Math.min(config.mapScale, Math.max(0, tmpObj.y));
 
         for (let x = 0; x < 10; x++) {
             let tmpX = x * tmpS;
@@ -21,13 +22,13 @@ export default class ObjectManager {
                 if (
                     objX + tmpObj.scale >= tmpX &&
                     objX - tmpObj.scale <= tmpX + tmpS &&
-					objY + tmpObj.scale >= tmpY &&
+                    objY + tmpObj.scale >= tmpY &&
                     objY - tmpObj.scale <= tmpY + tmpS
                 ) {
-					if (!this.grids[x + "_" + y]) this.grids[x + "_" + y] = [];
-					this.grids[x + "_" + y].push(tmpObj);
-					tmpObj.gridLocations.push(x + "_" + y);
-				}
+                    if (!this.grids[x + "_" + y]) this.grids[x + "_" + y] = [];
+                    this.grids[x + "_" + y].push(tmpObj);
+                    tmpObj.gridLocations.push(x + "_" + y);
+                }
             }
         }
     }
@@ -37,7 +38,7 @@ export default class ObjectManager {
         this.removeObjGrid(tmpObj);
     }
 
-    static removeObjGrid = function(obj) {
+    static removeObjGrid(obj) {
         for (let i = 0; i < obj.gridLocations.length; i++) {
             let indx = this.grids[obj.gridLocations[i]].indexOf(obj);
 
@@ -45,7 +46,12 @@ export default class ObjectManager {
                 this.grids[obj.gridLocations[i]].splice(indx, 1);
             }
         }
-	}
+    }
+
+    /**
+     * @param {GameObject} tmpObj 
+     * @param {number} tmpDir 
+     */
 
     static hitObj(tmpObj, tmpDir) {
         for (let i = 0; i < players.length; i++) {
@@ -65,124 +71,148 @@ export default class ObjectManager {
                 player.changeItemCount(tmpObj.group.id, -1);
             }
         }
-	}
+    }
+
+    /**
+     * @param {Player} player 
+     * @param {GameObject | player} other 
+     * @param {number} delta 
+     * @returns {boolean}
+     */
 
     static checkCollision(player, other, delta) {
-		delta = delta || 1;
+        delta = delta || 1;
 
-		let dx = player.x - other.x;
-		let dy = player.y - other.y;
-		let tmpLen = player.scale + other.scale;
+        let dx = player.x - other.x;
+        let dy = player.y - other.y;
+        let tmpLen = player.scale + other.scale;
 
-		if (Math.abs(dx) <= tmpLen || Math.abs(dy) <= tmpLen) {
-			tmpLen = player.scale + (other.getScale ? other.getScale() : other.scale);
+        if (Math.abs(dx) <= tmpLen || Math.abs(dy) <= tmpLen) {
+            tmpLen = player.scale + (other.getScale ? other.getScale() : other.scale);
 
-			let tmpInt = Math.sqrt(dx * dx + dy * dy) - tmpLen;
+            let tmpInt = Math.sqrt(dx * dx + dy * dy) - tmpLen;
 
-			if (tmpInt <= 0) {
-				if (!other.ignoreCollision) {
-					let tmpDir = UTILS.getDirection(player.x, player.y, other.x, other.y);
+            if (tmpInt <= 0) {
+                if (!other.ignoreCollision) {
+                    let tmpDir = UTILS.getDirection(player.x, player.y, other.x, other.y);
 
-					if (other.isPlayer) {
-						tmpInt = (tmpInt * -1) / 2;
-						player.x += (tmpInt * Math.cos(tmpDir));
-						player.y += (tmpInt * Math.sin(tmpDir));
-						other.x -= (tmpInt * Math.cos(tmpDir));
-						other.y -= (tmpInt * Math.sin(tmpDir));
-					} else {
-						player.x = other.x + (tmpLen * Math.cos(tmpDir));
-						player.y = other.y + (tmpLen * Math.sin(tmpDir));
-						player.xVel *= 0.75;
-						player.yVel *= 0.75;
-					}
+                    if (other.isPlayer) {
+                        tmpInt = (tmpInt * -1) / 2;
+                        player.x += (tmpInt * Math.cos(tmpDir));
+                        player.y += (tmpInt * Math.sin(tmpDir));
+                        other.x -= (tmpInt * Math.cos(tmpDir));
+                        other.y -= (tmpInt * Math.sin(tmpDir));
+                    } else {
+                        player.x = other.x + (tmpLen * Math.cos(tmpDir));
+                        player.y = other.y + (tmpLen * Math.sin(tmpDir));
+                        player.xVel *= 0.75;
+                        player.yVel *= 0.75;
+                    }
 
-					if (other.dmg && other.owner != player) {
-						player.changeHealth(-other.dmg, other.owner, other);
+                    if (other.dmg && other.owner != player) {
+                        player.changeHealth(-other.dmg, other.owner, other);
 
-						let tmpSpd = 1.5 * (other.weightM || 1);
+                        let tmpSpd = 1.5 * (other.weightM || 1);
 
-						player.xVel += tmpSpd * Math.cos(tmpDir);
-						player.yVel += tmpSpd * Math.sin(tmpDir);
-					}
-				} else if (other.trap && !player.noTrap && other.owner != player) {
-					player.lockMove = true;
-					other.hideFromEnemy = false;
-				} else if (other.boostSpeed) {
-					player.xVel += (delta * other.boostSpeed * (other.weightM||1)) * mathCOS(other.dir);
-					player.yVel += (delta * other.boostSpeed * (other.weightM||1)) * mathSIN(other.dir);
-				} else if (other.healCol) {
-					player.healCol = other.healCol;
-				} else if (other.teleport) {
-					player.x = UTILS.randInt(0, config.mapScale);
-					player.y = UTILS.randInt(0, config.mapScale);
-				}
+                        player.xVel += tmpSpd * Math.cos(tmpDir);
+                        player.yVel += tmpSpd * Math.sin(tmpDir);
+                    }
+                } else if (other.trap && !player.noTrap && other.owner != player) {
+                    player.lockMove = true;
+                    other.hideFromEnemy = false;
+                } else if (other.boostSpeed) {
+                    player.xVel += (delta * other.boostSpeed * (other.weightM || 1)) * mathCOS(other.dir);
+                    player.yVel += (delta * other.boostSpeed * (other.weightM || 1)) * mathSIN(other.dir);
+                } else if (other.healCol) {
+                    player.healCol = other.healCol;
+                } else if (other.teleport) {
+                    player.x = UTILS.randInt(0, config.mapScale);
+                    player.y = UTILS.randInt(0, config.mapScale);
+                }
 
-				if (other.zIndex > player.zIndex) player.zIndex = other.zIndex;
+                if (other.zIndex > player.zIndex) player.zIndex = other.zIndex;
 
-				return true;
-			}
-		}
-		return false;
-	}
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param {number} xPos 
+     * @param {number} yPos 
+     * @param {number} s - Scale of the Entity
+     * @returns {GameObject[][]}
+     */
 
     static getGridArrays(xPos, yPos, s) {
         let tmpS = 1440;
         let tmpX = Math.floor(xPos / tmpS);
-		let tmpY = Math.floor(yPos / tmpS);
-		let tmpArray = [];
+        let tmpY = Math.floor(yPos / tmpS);
+        let tmpArray = [];
 
-		try {
-			if (this.grids[tmpX + "_" + tmpY]) tmpArray.push(this.grids[tmpX + "_" + tmpY]);
+        try {
+            if (this.grids[tmpX + "_" + tmpY]) tmpArray.push(this.grids[tmpX + "_" + tmpY]);
 
-			if (xPos + s >= (tmpX + 1) * tmpS) {
-				tmpGrid = this.grids[(tmpX + 1) + "_" + tmpY];
+            if (xPos + s >= (tmpX + 1) * tmpS) {
+                tmpGrid = this.grids[(tmpX + 1) + "_" + tmpY];
 
-				if (tmpGrid) tmpArray.push(tmpGrid);
+                if (tmpGrid) tmpArray.push(tmpGrid);
 
-				if (tmpY && yPos - s <= tmpY * tmpS) {
-					tmpGrid = this.grids[(tmpX + 1) + "_" + (tmpY - 1)];
+                if (tmpY && yPos - s <= tmpY * tmpS) {
+                    tmpGrid = this.grids[(tmpX + 1) + "_" + (tmpY - 1)];
 
-					if (tmpGrid) tmpArray.push(tmpGrid);
-				} else if (yPos + s >= (tmpY + 1) * tmpS) {
-					tmpGrid = this.grids[(tmpX + 1) + "_" + (tmpY + 1)];
+                    if (tmpGrid) tmpArray.push(tmpGrid);
+                } else if (yPos + s >= (tmpY + 1) * tmpS) {
+                    tmpGrid = this.grids[(tmpX + 1) + "_" + (tmpY + 1)];
 
-					if (tmpGrid) tmpArray.push(tmpGrid);
-				}
-			}
-            
+                    if (tmpGrid) tmpArray.push(tmpGrid);
+                }
+            }
+
             if (tmpX && xPos - s <= tmpX * tmpS) {
-				tmpGrid = this.grids[(tmpX - 1) + "_" + tmpY];
+                tmpGrid = this.grids[(tmpX - 1) + "_" + tmpY];
 
-				if (tmpGrid) tmpArray.push(tmpGrid);
+                if (tmpGrid) tmpArray.push(tmpGrid);
 
-				if (tmpY && yPos - s <= tmpY * tmpS) {
-					tmpGrid = this.grids[(tmpX - 1) + "_" + (tmpY - 1)];
+                if (tmpY && yPos - s <= tmpY * tmpS) {
+                    tmpGrid = this.grids[(tmpX - 1) + "_" + (tmpY - 1)];
 
-					if (tmpGrid) tmpArray.push(tmpGrid);
-				} else if (yPos + s >= (tmpY + 1) * tmpS) {
-					tmpGrid = this.grids[(tmpX - 1) + "_" + (tmpY + 1)];
+                    if (tmpGrid) tmpArray.push(tmpGrid);
+                } else if (yPos + s >= (tmpY + 1) * tmpS) {
+                    tmpGrid = this.grids[(tmpX - 1) + "_" + (tmpY + 1)];
 
-					if (tmpGrid) tmpArray.push(tmpGrid);
-				}
-			}
-            
+                    if (tmpGrid) tmpArray.push(tmpGrid);
+                }
+            }
+
             if (yPos + s >= (tmpY + 1) * tmpS) {
-				tmpGrid = this.grids[tmpX + "_" + (tmpY + 1)];
+                tmpGrid = this.grids[tmpX + "_" + (tmpY + 1)];
 
-				if (tmpGrid) tmpArray.push(tmpGrid);
-			}
-            
+                if (tmpGrid) tmpArray.push(tmpGrid);
+            }
+
             if (tmpY && yPos - s <= tmpY * tmpS) {
-				tmpGrid = this.grids[tmpX + "_" + (tmpY - 1)];
+                tmpGrid = this.grids[tmpX + "_" + (tmpY - 1)];
 
-				if (tmpGrid) tmpArray.push(tmpGrid);
-			}
-		} catch (e) {}
+                if (tmpGrid) tmpArray.push(tmpGrid);
+            }
+        } catch (e) { }
 
-		return tmpArray;
+        return tmpArray;
     }
 
-    static checkItem(x, y, s, sM, indx, ignoreWater, placer) {
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} s 
+     * @param {number} sM 
+     * @param {number} indx 
+     * @param {boolean} ignoreWater 
+     * @returns {boolean}
+     */
+
+    static checkItem(x, y, s, sM, indx, ignoreWater) {
         for (let i = 0; i < gameObjects.length; i++) {
             let tmpObj = gameObjects[i];
 
@@ -201,6 +231,19 @@ export default class ObjectManager {
 
         return true;
     }
+
+    /**
+     * 
+     * @param {number} sid 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} dir 
+     * @param {number} s 
+     * @param {number} type 
+     * @param {(string | number)[]} data 
+     * @param {number} setSID 
+     * @param {Player} owner 
+     */
 
     static add(sid, x, y, dir, s, type, data, setSID, owner) {
         let tmpObj;

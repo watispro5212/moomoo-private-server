6dbfb2ac41537be646c42818dceb6116a776217b
me-mega-noob-4044 <testtrolium@gmail.com>
2025-03-30 23:02:33 -0400

    d

--- src/logic/ProjectileManager.js
+++ src/logic/ProjectileManager.js
@@ -1,29 +1,44 @@
 import items from "../constants/items.js";
 import Projectile from "./Projectile.js";
 import { projectiles } from "../../index.js";
+import Player from "./Players.js";
 
 export default class ProjectileManager {
-    static addProjectile(x, y, dir, range, speed, indx, owner, ignoreObj, layer) {
-        let tmpData = items.projectiles[indx];
-	    let tmpProj;
 
-	    for (var i = 0; i < projectiles.length; ++i) {
-	        if (!projectiles[i].active) {
-	            tmpProj = projectiles[i];
-	            break;
-	        }
-	    }
-        
-        if (!tmpProj) {
-	        tmpProj = new Projectile();
+	/**
+	 * @param {number} x 
+	 * @param {number} y 
+	 * @param {number} dir 
+	 * @param {number} range 
+	 * @param {number} speed 
+	 * @param {number} indx 
+	 * @param {Player} owner 
+	 * @param {boolean | number} ignoreObj 
+	 * @param {number} layer 
+	 * @returns {Projectile}
+	 */
+
+	static addProjectile(x, y, dir, range, speed, indx, owner, ignoreObj, layer) {
+		let tmpData = items.projectiles[indx];
+		let tmpProj;
+
+		for (var i = 0; i < projectiles.length; ++i) {
+			if (!projectiles[i].active) {
+				tmpProj = projectiles[i];
+				break;
+			}
+		}
+
+		if (!tmpProj) {
+			tmpProj = new Projectile();
 			tmpProj.sid = projectiles.length;
-	        projectiles.push(tmpProj);
-	    }
+			projectiles.push(tmpProj);
+		}
 
 		tmpProj.init(indx, x, y, dir, speed, tmpData.dmg, range, tmpData.scale, owner);
 		tmpProj.ignoreObj = ignoreObj;
 		tmpProj.layer = layer || tmpData.layer;
 
 		return tmpProj;
-    }
+	}
 }
